5d362261051e6fa5b2bc16e941ba83040af01a84
Justin <justinconforti@gmail.com>
2019-07-29 14:07:25 -0400

    starting view.js portion to connect HTML

--- controller/burgerController.js
+++ controller/burgerController.js
@@ -3,11 +3,15 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var burger = require("../models/burger.js");
+
+var db = require("../models");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-    res.render("index");
+    db.Burger.findAll({}).then(function(results){
+        console.log("results here" +results)
+    })
+    res.render("index", {burgers: results});
 });
 
 // Export routes for server.js to use.
